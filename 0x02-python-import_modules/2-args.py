f61b816ed689dcf9c3a05996fb7dd86248a061a7
mionki <piousmionki@gmail.com>
2022-06-02 15:54:26 +0300

    3rd task fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     elif args == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(count))
+        print("{} arguments:".format(args))
     for i in range(args):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
