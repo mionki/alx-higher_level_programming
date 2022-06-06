81e133c5419bd654ceddcdad1da2e8733d78b2fe
mionki <piousmionki@gmail.com>
2022-06-06 11:26:41 +0300

    6th task

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ''
+    for word in my_string:
+        if word != 'c' and word != 'C':
+            new_string += word
+    return new_string        
