3d281bca5746bd7fc1f7275f29802306a34c16a6
mionki <piousmionki@gmail.com>
2022-06-06 10:33:22 +0300

    task 2 corrections

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    my_list = []
-    for idx in range(len(my_list)-1):
-        print("Element at index {:d} is {}".format(idx,my_list[idx]))
-        if idx < 0:
-            return None
-        if idx > len(my_list):
-            return None    
+    """retrive an element from list"""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
+    return (my_list[idx])
