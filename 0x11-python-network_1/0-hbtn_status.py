62081bc012a455780ae60c073e105b092cad2afe
mionki <piousmionki@gmail.com>
2022-09-23 17:15:51 +0300

    corrections5

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-fetches https://intranet.hbtn.io/status
+fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         html = response.read()
         print('Body response:')
